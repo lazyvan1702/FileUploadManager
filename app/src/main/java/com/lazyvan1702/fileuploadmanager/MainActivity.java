01735047abbe268ad81a5cef3b4ae855b6b45af2
lazyvan1702 <lazyvan1702@gmail.com>
2017-02-19 17:22:32 +0900

    first commit
    Android Nougat project
    Create default blank activity

---
+++ app/src/main/java/com/lazyvan1702/fileuploadmanager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lazyvan1702.fileuploadmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
